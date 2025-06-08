4c5010017f1f03c4ef62b5df66cd2ef053bb1894
Harini-hyd <harinisrik346@gmail.com>
2025-06-08 19:31:16 +0530

    Initial Commit

---
+++ src/main/java/com/pc/maven6/App.java
@@ -0,0 +1,13 @@
+package com.pc.maven6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
